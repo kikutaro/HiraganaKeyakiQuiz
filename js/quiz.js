834d283bfde3313ef3d921a583de486b222ebb6e
kikutaro <kikutaro526@yahoo.co.jp>
2018-03-04 01:47:36 +0900

    :loudspeaker: First commit.

---
+++ js/quiz.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $('#q1a1').click(function() {
+        $('.ui.basic.modal.cross').modal({
+            centered: true
+        }).modal('show');
+    });
+    $('#q1a2').click(function() {
+        $('.ui.basic.modal.cross').modal({
+            centered: true
+        }).modal('show');
+    });
+    $('#q1a3').click(function() {
+        $('.ui.basic.modal.tick').modal({
+            centered: true
+        }).modal('show');
+    });
+    $('#q1a4').click(function() {
+        $('.ui.basic.modal.cross').modal({
+            centered: true
+        }).modal('show');
+    });
+    $('#question').transition({
+        debug     : true,
+        animation : 'pulse',
+        duration  : 1000,
+        interval  : 200
+    });
+});
