6b180a126f58edffb65a4e03f6e6794a53172bd7
kikutaro <kikutaro526@yahoo.co.jp>
2018-03-18 17:27:30 +0900

    Vue routerによるURLパラメータのハンドリング処理を追加

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 Vue.component("quiz", {
-  props: ["id", "name", "question", "image", "answer1", "answer2", "answer3", "answer4"],
+  props: ["id", "no", "name", "question", "image", "answer1", "answer2", "answer3", "answer4"],
   template: 
   `
   <div id="question" class="ui card">
@@ -14,22 +14,22 @@ Vue.component("quiz", {
     <div class="content">
         <div class="ui stackable two column grid">
             <div class="column">
-                <button id="q1a1" class="fluid ui button inverted big green" v-on:click="check(id, 1)">1.{{answer1}}</button>
+                <button class="fluid ui button inverted big green" v-on:click="check(no, 1)">1.{{answer1}}</button>
             </div>
             <div class="column">
-                <button id="q1a2" class="fluid ui button inverted big green" v-on:click="check(id, 2)">2.{{answer2}}</button>
+                <button class="fluid ui button inverted big green" v-on:click="check(no, 2)">2.{{answer2}}</button>
             </div>
             <div class="column">
-                <button id="q1a3" class="fluid ui button inverted big green" v-on:click="check(id, 3)">3.{{answer3}}</button>
+                <button class="fluid ui button inverted big green" v-on:click="check(no, 3)">3.{{answer3}}</button>
             </div>
             <div class="column">
-                <button id="q1a4" class="fluid ui button inverted big green" v-on:click="check(id, 4)">4.{{answer4}}</button>
+                <button class="fluid ui button inverted big green" v-on:click="check(no, 4)">4.{{answer4}}</button>
             </div>
         </div>
     </div>
     <div class="extra content">
         <form>
-            <a class="ui circular twitter icon button" v-bind:href="'https://twitter.com/intent/tweet?hashtags=ひらがなけやき2期生クイズ%2C' + name">
+            <a class="ui circular twitter icon button" v-bind:href="'https://twitter.com/intent/tweet?text=' + question + '&url=http%3A%2F%2Flocalhost:8080%2Fquiz?no=' + no + '&hashtags=ひらがなけやき2期生クイズ%2C' + name" target="_blank">
                 <i class="twitter icon"></i>
             </a>
             <button class="ui circular facebook icon button">
@@ -37,7 +37,7 @@ Vue.component("quiz", {
             </button>
         </form>
     </div>
-    <div v-bind:id="id + 'tick'" class="ui basic modal tick">
+    <div v-bind:id="no + 'tick'" class="ui basic modal tick">
       <div class="ui icon header">
         <i class="green smile icon"></i>
         正解
@@ -52,7 +52,7 @@ Vue.component("quiz", {
         </div>
       </div>
     </div>
-    <div v-bind:id="id + 'cross'" class="ui basic modal cross">
+    <div v-bind:id="no + 'cross'" class="ui basic modal cross">
       <div class="ui icon header">
         <i class="green frown outline icon"></i>
         不正解
@@ -72,16 +72,16 @@ Vue.component("quiz", {
     }
   },
   methods: {
-    check: function(id, answer_no) {
-      axios.get("http://localhost:8080/quiz/answer?id=" + id + "&answer=" + answer_no)
+    check: function(no, answer_no) {
+      axios.get("http://localhost:8080/quiz/answer?no=" + no + "&answer=" + answer_no)
       .then((response) => {
         this.answer = response.data;
         if(response.data.correct) {
-          $('#'+id+'tick').modal({
+          $('#'+no+'tick').modal({
             centered: true
           }).modal('show');
         } else if(!response.data.correct) {
-          $('#'+id+'cross').modal({
+          $('#'+no+'cross').modal({
             centered: true
           }).modal('show');
         }
@@ -90,9 +90,16 @@ Vue.component("quiz", {
   }
 });
 
+const routes = []
+const router = new VueRouter({
+  mode: 'history',
+  routes
+});
+
 var app = new Vue({
   el: '#app',
   data: {
+    error: false,
     quizs: [],
     members: [
       { key: 'kanemuramiku', name: '金村美玖'},{ key: 'kawatahina', name: '河田陽菜'},
@@ -111,9 +118,25 @@ var app = new Vue({
     }
   },
   mounted() {
-    axios.get("http://localhost:8080/quiz?no=" + Math.floor(Math.random() * 4))
-    .then((response) => {
-      this.quizs = Array.of(response.data);
-    })
+    console.log(this.error)
+    console.log(this.$route.query.no)
+    if(typeof this.$route.query.no === 'undefined' || isNaN(this.$route.query.no)) {
+      axios.get("http://localhost:8080/quiz/random")
+      .then((response) => {
+        this.quizs = Array.of(response.data);
+      })
+    } else {
+      axios.get("http://localhost:8080/quiz?no=" + this.$route.query.no)
+      .then((response) => {
+        console.log(response.data)
+        this.quizs = Array.of(response.data);
+      })
+      .catch((response) => {
+        this.error = true;
+        console.log("error")
+      })
+    }
   }
+  ,
+  router
 })
